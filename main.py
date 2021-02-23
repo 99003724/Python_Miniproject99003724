028fd41f8a28efce00d55b1a43157a6df2c09490
99003724 <78853934+99003724@users.noreply.github.com>
2021-02-23 14:41:37 +0530

    code added

---
+++ main.py
@@ -0,0 +1,17 @@
+import re
+count = 0
+software =[]
+lines = 0
+
+pattern = re.compile("software", re.IGNORECASE)
+with open("D:\project.txt","rt") as file_info:
+    for file_line in file_info:
+        lines+=1
+        if pattern.search(file_line)!=None:
+            software.append((lines,file_line.rstrip('\n')))
+    for answer in software:
+        count+=1
+        with open("software.txt","a") as file_answer:
+            file_answer.writelines(str(count)+" :")
+            file_answer.writelines(answer[1]+'\n')
+
