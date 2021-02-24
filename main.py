a232bc7734347406b215908744f137420d416a8d
99003724 <78853934+99003724@users.noreply.github.com>
2021-02-24 09:29:07 +0530

    code added

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import re
-count = 0
-software =[]
-lines = 0
 
-pattern = re.compile("software", re.IGNORECASE)
-with open("D:\project.txt","rt") as file_info:
-    for file_line in file_info:
-        lines+=1
-        if pattern.search(file_line)!=None:
-            software.append((lines,file_line.rstrip('\n')))
-    for answer in software:
-        count+=1
-        with open("software.txt","a") as file_answer:
-            file_answer.writelines(str(count)+" :")
-            file_answer.writelines(answer[1]+'\n')
+class text_processing:
 
+    def __init__(self,search_word):
+        self.file_i = open("D:\\project.txt","r").read()
+        self.search_word = search_word
+
+
+    def search_word_inf(self):
+        num_out = re.findall(self.search_word,self.file_i,re.M | re.I)
+        return num_out
+
+def out_file(search_word,num_out):
+        new_file = search_word + "project.txt"
+        new_out_file= open(new_file,"a")
+        new_out_file.write("count of word: ")
+        new_out_file.write(str(len(num_out)))
+        new_out_file.write("\n")
+        new_out_file.write(word_search[1]+'\n') 
+
+if __name__ == "__main__":
+    word_search = input("Enter the word: ")
+    find = text_processing(word_search)
+    count = find.search_word_inf()
+    out_file(word_search,count)
