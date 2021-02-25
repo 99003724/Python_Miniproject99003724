8f4d40acf7c401722da42975367f8ee1c59b4543
99003724 <78853934+99003724@users.noreply.github.com>
2021-02-25 12:50:25 +0530

    edited

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ class SearchInputWord:
         new_out_file.write(str("\n"))
         sp_t = self.file_in.split()
         # Using loop to find the words before and after the input word.
-        for i in range(len(split_text)):
+        for i in range(len(sp_t)):
             match = re.match(self.search_word, sp_t[i], re.M | re.I)
             if match:
                 count += 1
